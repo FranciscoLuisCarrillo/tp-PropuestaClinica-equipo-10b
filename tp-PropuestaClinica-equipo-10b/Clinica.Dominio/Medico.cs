38ec46afd588faadc61a374e45cd8bebca1cfe28
Stepanuck <marinozzitomas@hotmail.com>
2025-10-25 22:04:12 -0300

    TM - 1 - 1.02 Clases
    Generacion de algunas clases

---
+++ tp-PropuestaClinica-equipo-10b/Clinica.Dominio/Medico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica.Dominio
+{
+    public class Medico : Persona
+    {
+        string Matricula { get; set; }
+        string Especialidad { get; set; }
+    }
+}
---
+++ tp-PropuestaClinica-equipo-10b/Clinica.Dominio/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica.Dominio
+{
+    public class Persona
+    {
+        int Id { get; set; }
+        string Nombre { get; set; }
+        string Apellido { get; set; }
+        DateTime FechaNacimiento { get; set; }
+        string Genero { get; set; }
+        string Dni { get; set; }
+        string Direccion { get; set; }
+        string Telefono { get; set; }
+        string Email { get; set; }
+
+
+    }
+}
