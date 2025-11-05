6beed6ee56f807448dfb8e94a745a703abcf9d7d
CARRILLO FRANCISCO LUIS <carrillo.francisco.luis@gmail.com>
2025-11-05 12:55:21 -0300

    CF - Se elimino TestManager

--- tp-PropuestaClinica-equipo-10b/presentacion/Dashboard.aspx
+++ tp-PropuestaClinica-equipo-10b/presentacion/Dashboard.aspx
@@ -1,5 +1,4 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/App.Master" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="Presentacion.Dashboard" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-</asp:Content>
+
--- tp-PropuestaClinica-equipo-10b/presentacion/Dashboard.aspx.cs
+++ tp-PropuestaClinica-equipo-10b/presentacion/Dashboard.aspx.cs
@@ -1,27 +1,18 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Clinica.Negocio.Pruebas;
+// BORRA ESTA LÍNEA: using Clinica.Negocio.Pruebas; 
 namespace Presentacion
 {
     public partial class Dashboard : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // --- PRUEBA 1: AGREGAR ESPECIALIDAD NUEVA ---
-            TestManager.ProbarAgregarEspecialidad("Odontología");
-
-            // --- PRUEBA 2: AGREGAR LA MISMA ESPECIALIDAD (debería fallar la validación) ---
-            TestManager.ProbarAgregarEspecialidad("odontología"); // Prueba case-insensitivity
-
-            // --- PRUEBA 3: AGREGAR OTRA NUEVA ESPECIALIDAD ---
-            TestManager.ProbarAgregarEspecialidad("Cardiología");
-
-            // --- PRUEBA 4: NOMBRE VACÍO (debería lanzar ArgumentException) ---
-            TestManager.ProbarAgregarEspecialidad(string.Empty);
+            // --- BORRA TODO EL CÓDIGO DE PRUEBA DE AQUÍ ---
+            // (Originalmente aquí estaban las llamadas a TestManager)
         }
     }
 }
